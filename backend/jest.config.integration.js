5ed9e48b50b97adb3f9e36f97afebde821dbf7d7
Simonkho <63922818+Simonkrh@users.noreply.github.com>
2024-11-13 11:36:57 +0100

    Refactor backend tests.

---
+++ backend/jest.config.integration.js
@@ -0,0 +1,5 @@
+/*global module*/
+module.exports = {
+    testMatch: ['**/tests/integration/**/*.test.js'], // Only run tests in the integration folder
+  };
+  
---
+++ backend/jest.config.unit.js
@@ -0,0 +1,5 @@
+/*global module*/
+module.exports = {
+    testMatch: ['**/tests/unit/**/*.test.js'], // Only run tests in the unit folder
+  };
+  
